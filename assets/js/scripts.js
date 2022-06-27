86d0e7425c748065c5e99ce6f2e0b3451e9c352a
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-27 23:42:58 +0700

    Finish Quincy Configuration

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,3 +13,24 @@ closeBar.addEventListener("click", function () {
   menuBar.classList.add("show-menu");
   navbar.classList.remove("active");
 });
+
+const navItems = document.querySelectorAll(".nav-item");
+
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", function () {
+    // since you are only going to have one element
+    // with active class use querySelector() as it
+    // will return the first element with this class
+    // no need to reference an index
+    // provided you already have an active class in your DOM
+
+    var current = document.querySelector(".active");
+    // let s make sure there is a current active class
+    // before trying to remove it from DOM
+    if (current) {
+      // then use classList to add/remove class names
+      current.classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
